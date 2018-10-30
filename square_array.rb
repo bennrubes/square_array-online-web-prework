1b18f36151865215bb78a222ee4cbe60db6ea5d6
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 20:27:50 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  sq_numbers = array
+  sq_numbers = []
  
-  sq_numbers.each do |array|
-    (array) ** 2
+  sq_numbers.each do |num|
+    sq_numbers.push(num ** 2)
   end
-  
+  sq_numbers
 end
